5ab1cc8d5636296ad30147b6d8316aa848300d76
= <benjamin.schwab@duke.edu>
2014-03-31 23:07:28 -0400

    temp junk

---
+++ app/App revolutionizes awkward STD conversations   The Chronicle_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Comments (0)","multiple":"Comments ({num})","one":"Comments (1)"}},"counts":[{"id":"_151284","comments":0}]});
+}
